a67579a67b138e4865a69c7da3697e586ab87808
phadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-02 13:56:40 +0530

    adding program to remove words from paragraph

--- src/main/java/com/datastructure/HashTable/HashTableMain.java
+++ src/main/java/com/datastructure/HashTable/HashTableMain.java
@@ -15,16 +15,23 @@ public class HashTableMain {
         for (String word : sentenceArray) {
             Integer value = hashTable.get(word);
 
-            if( value == null)
+            if( value == null) // //frequency of the word StayWith 1
                 value = 1;
             else
-                value = value + 1;
+                value = value + 1; //frequency of the word increases by 1
             hashTable.add(word , value);
+            System.out.println("Value = "+value);
         }
         System.out.println(hashTable);
-	}
 
+        //Remove "avoidable" from the hashtable
+        hashTable.remove("avoidable");
+
+        System.out.println(hashTable);
+    }
 }
+
+	
 	
 		
 	
